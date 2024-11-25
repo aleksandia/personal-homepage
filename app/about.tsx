9b25baa4bb528dafa969170d2d03b009a1baf046
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-11-25 20:57:29 +0100

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,1 +1,17 @@
+import { BlogPosts } from 'app/components/posts'
 
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        About me
+      </h1>
+      <p className="mb-4">
+        {`I am Aleksandar a student at Johannes Gutenberg-University Mainz`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
