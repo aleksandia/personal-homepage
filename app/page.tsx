f2efb98305138e76f2b7172e846f8c553b246626
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-11-25 22:09:44 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Page() {
       <p className="mb-4">
         {`Welcome to my website!`}
       </p>
-      <div className="my-8">
-      - First item
-- Second item
-- Third item
-- Fourth item
-</div>
+      <ul>
+  <li>First item</li>
+  <li>Second item</li>
+  <li>Third item</li>
+  <li>Fourth item</li>
+</ul>
       <div className="my-8">
         <BlogPosts />
       </div>
