49bcee81a8cde3ff54ba54511bfe5c9722255bdb
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-12-07 23:31:48 +0100

    mail added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,13 @@
 const About = () => {
   return (
     <div>
-     <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Reach out
       </h1>
       <p>I'd love to hear from you! You can send me an e-mail at: </p>
+      <a href="mailto:your-email@example.com" className="text-blue-500 underline">
+        your-email@example.com
+      </a>
     </div>
   );
 };
