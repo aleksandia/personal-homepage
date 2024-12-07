923717f8999df4f09225fcbab34b69d9ede34f10
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-12-07 23:20:28 +0100

    contact page updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,11 @@
 const Contact = () => {
     return (
       <div>
-        <h1>Contact Page</h1>
-        <p>This is the contact page.</p>
+         <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Reach out
+      </h1>
+      <p>I'd love to hear from you!</p><br>
+      <p>You can drop me a message at <a href="mailto:urosevic@students.uni-mainz.de">urosevic@students.uni-mainz.de</a></p>
       </div>
     );
   };
