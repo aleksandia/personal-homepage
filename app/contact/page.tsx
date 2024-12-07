b0d542cc5005aa914799f005ded34e91be163286
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-12-07 23:25:28 +0100

    2nd  update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,12 @@
-const Contact = () => {
-    return (
-      <div>
-         <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+const About = () => {
+  return (
+    <div>
+     <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Reach out
       </h1>
-      <p>I'd love to hear from you!</p><br>
-      <p>You can drop me a message at urosevic@students.uni-mainz.de </p>
-      </div>
-    );
-  };
-  
-  export default Contact;
-  
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
