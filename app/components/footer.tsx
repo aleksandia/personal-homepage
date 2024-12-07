6d0eaad9c9a2e90f90c22ea31398f801fe6380b2
aleksandia <98323601+aleksandia@users.noreply.github.com>
2024-12-07 23:13:33 +0100

    computersammlung added to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,6 +46,17 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
+            href="https://www.sammlungen.uni-mainz.de/computersammlung/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">computersammlung</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
             href="http://urosevic.de/tech-wiki"
           >
             <ArrowIcon />
