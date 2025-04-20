bb159dcf599bccb5fad5563bcaffdfb807962d04
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-04-20 16:14:36 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,44 @@
+import fs from 'fs'
+import path from 'path'
 import { getBlogPosts } from 'app/blog/utils'
 
 export const baseUrl = 'https://urosevic.vercel.app'
 
+function getStaticPages() {
+  const dir = path.join(process.cwd(), 'app') // entry point of your app directory
+  const entries = fs.readdirSync(dir, { withFileTypes: true })
+
+  const ignored = new Set(['blog', 'components', 'public', 'og', 'rss', 'api'])
+
+  const routes = entries.flatMap((entry) => {
+    const fullPath = path.join(dir, entry.name)
+    const pageFile = path.join(fullPath, 'page.tsx')
+
+    if (
+      entry.isDirectory() &&
+      !ignored.has(entry.name) &&
+      fs.existsSync(pageFile)
+    ) {
+      return `/${entry.name}`
+    }
+
+    return []
+  })
+
+  // Add the homepage `/` manually
+  return ['/', ...routes]
+}
+
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
+  const blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  const staticPages = getStaticPages().map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...staticPages, ...blogs]
 }
