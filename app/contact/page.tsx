a9bd4223294c24a171bd92ee90bf4177e7c091a9
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-09-06 21:19:19 +0200

    Update page.tsx
    another try again

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,37 +1,27 @@
-import Head from "next/head";
+// app/contact/page.tsx
+import { Metadata } from "next";
 
-const About = () => {
-  return (
-    <>
-      <Head>
-        {/* Add all your meta tags here */}
-        <meta
-          name="google-site-verification"
-          content="Qw2MRfXNWiiOKsUL4-q9z7cm17nXRU6LCgQCe9ZDrjE"
-        />
-        <title>Contact - Aleksandar Urosevic</title>
-        <meta
-          name="description"
-          content="Personal homepage of Aleksandar Urosevic"
-        />
-        <meta name="author" content="Aleksandar Urosevic" />
-        <link rel="canonical" href="https://urosevic.vercel.app/" />
-        <meta httpEquiv="content-language" content="en" />
-        <meta name="robots" content="index, follow" />
-      </Head>
-
-      <div>
-        <h1 className="text-4xl font-bold mb-6">Reaching out to me</h1>
-        <p>I'd love to hear from you! You can send me an e-mail at:</p>
-        <a
-          href="mailto:urosevic@students.uni-mainz.de"
-          className="text-blue-500 underline"
-        >
-          urosevic@students.uni-mainz.de
-        </a>
-      </div>
-    </>
-  );
+export const metadata: Metadata = {
+  title: "Contact - Aleksandar Urosevic",
+  description: "Personal homepage of Aleksandar Urosevic",
+  authors: [{ name: "Aleksandar Urosevic" }],
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://urosevic.vercel.app/contact",
+  },
 };
 
-export default About;
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-6">Reaching out to me</h1>
+      <p>I'd love to hear from you! You can send me an e-mail at:</p>
+      <a
+        href="mailto:urosevic@students.uni-mainz.de"
+        className="text-blue-500 underline"
+      >
+        urosevic@students.uni-mainz.de
+      </a>
+    </div>
+  );
+}
