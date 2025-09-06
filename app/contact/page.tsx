ed17a47ee9c4cf046c5a7e696062cc75db5203de
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-09-06 21:07:41 +0200

    Update page.tsx
    changed header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,7 @@
 const About = () => {
   return (
     <div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Reach out
-      </h1>
+       <h1 className="text-4xl font-bold mb-6">Reaching out to me</h1>
       <p>I'd love to hear from you! You can send me an e-mail at: </p>
       <a href="mailto:urosevic@students.uni-mainz.de" className="text-blue-500 underline">
       urosevic@students.uni-mainz.de
@@ -13,3 +11,4 @@ const About = () => {
 };
 
 export default About;
+
