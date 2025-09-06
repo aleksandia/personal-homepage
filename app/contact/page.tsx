5cdb70c922a1eeeec914f4452542c3d6870d6dbc
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-09-06 21:10:00 +0200

    Update page.tsx
    change of title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,19 @@
 const About = () => {
   return (
+      <Head>
+        {/* Add all your meta tags here */}
+        <meta
+          name="google-site-verification"
+          content="Qw2MRfXNWiiOKsUL4-q9z7cm17nXRU6LCgQCe9ZDrjE"
+        />
+        <title>Contact - Aleksandar Urosevic</title>
+        <meta name="description" content="Personal homepage of Aleksandar Urosevic" />
+        <meta name="author" content="Aleksandar Urosevic" />
+        <link rel="canonical" href="https://urosevic.vercel.app/" />
+        <meta http-equiv="content-language" content="en" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+    
     <div>
        <h1 className="text-4xl font-bold mb-6">Reaching out to me</h1>
       <p>I'd love to hear from you! You can send me an e-mail at: </p>
@@ -11,4 +25,5 @@ const About = () => {
 };
 
 export default About;
+
 
