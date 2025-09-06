aaf6ec43df9655e2221db5d20e8092ed86345c7f
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-09-06 21:03:17 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,16 @@
 const About = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-6">
-  About Me
-</h1>
-      <p>Hi, I’m Aleksandar — welcome to my personal homepage!
-        <br>
-I’m an undergraduate student studying economics at Johannes Gutenberg University Mainz and work as a student assistant sysadmin at the chair of Prof. Dr. Franz Rothlauf (Information Systems and Business Administration).
-<br>
-Outside of my studies and work, I spend a lot of time with various IT projects. I run several self-hosted services on my own hardware and enjoy playing around with telephony systems such as Cisco CallManager, HiCom, and HiPath.
-<br>
-Other than that I like to go for walks and cycling.</p>
+      <h1 className="text-4xl font-bold mb-6">About Me</h1>
+      <p>
+        Hi, I’m Aleksandar — welcome to my personal homepage!
+        <br /><br />
+        I’m an undergraduate student in Economics at Johannes Gutenberg University Mainz and work as a student assistant in system administration at the Chair of Prof. Dr. Franz Rothlauf (Information Systems and Business Administration).
+        <br /><br />
+        Outside of my studies and work, I spend a lot of time with various IT projects. I run several self-hosted services on my own hardware and enjoy experimenting with telephony systems such as Cisco CallManager, HiCom, and HiPath.
+        <br /><br />
+        Beyond technology, I like going for walks and cycling.
+      </p>
     </div>
   );
 };
