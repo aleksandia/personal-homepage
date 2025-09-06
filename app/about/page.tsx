e2660645ab488869237a48725364d0d3f043b815
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-09-06 21:01:37 +0200

    Update page.tsx
    created a description about myself.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,16 @@
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This is the about page.</p>
+      <h1 className="text-4xl font-bold mb-6">
+  About Me
+</h1>
+      <p>Hi, I’m Aleksandar — welcome to my personal homepage!
+        <br>
+I’m an undergraduate student studying economics at Johannes Gutenberg University Mainz and work as a student assistant sysadmin at the chair of Prof. Dr. Franz Rothlauf (Information Systems and Business Administration).
+<br>
+Outside of my studies and work, I spend a lot of time with various IT projects. I run several self-hosted services on my own hardware and enjoy playing around with telephony systems such as Cisco CallManager, HiCom, and HiPath.
+<br>
+Other than that I like to go for walks and cycling.</p>
     </div>
   );
 };
