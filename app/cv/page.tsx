74f0bbd672e1ec50dcc65c4c29dedea218342f91
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-11-27 09:04:52 +0100

    Add download link for CV PDF

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -15,6 +15,13 @@ export default function ContactPage() {
   return (
      <>
       <h1 className="text-4xl font-bold mb-6">Curriculum vitae</h1>
+          <a
+      href="/cv.pdf"
+      download
+      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+    >
+      Download PDF
+    </a>
     <main className="cv-container">
 
 
